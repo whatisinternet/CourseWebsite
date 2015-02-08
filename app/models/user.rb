69657d7d1a68566004778f30e292ce27fc40ccc2
Whatisinternet <joshteeter@gmail.com>
2015-02-08 01:29:03 -0500

    Added validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_format_of :email, :with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+  validates :email, :password, :presence => true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "should have valid factory" do
+	    expect(FactoryGirl.build(:user)).to be_valid
+	end
+
+	it "should require a email" do
+	    expect(FactoryGirl.build(:user, :email => "", :password => Faker::Internet.password(10, 20) )).to_not be_valid
+	end
+
+	it "should require a password" do
+	    expect(FactoryGirl.build(:user, :email => Faker::Internet.email, :password => "")).to_not be_valid
+	end
+
+	it "should require a valid email" do
+	    expect(FactoryGirl.build(:user, :email => "inv@lid@email@@.x(", :password =>  Faker::Internet.password(10, 20) )).to_not be_valid
+	end
+
+	it "should require a valid password" do
+	    expect(FactoryGirl.build(:user, :email => Faker::Internet.email, :password => "-1")).to_not be_valid
+	end
+
 end
