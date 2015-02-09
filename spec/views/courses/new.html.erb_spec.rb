02cdf436cebf02ce4345e9df0877c355d6737a73
Whatisinternet <joshteeter@gmail.com>
2015-02-09 18:36:47 -0500

    Fixed courses view to include correct data

--- spec/views/courses/new.html.erb_spec.rb
+++ spec/views/courses/new.html.erb_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "courses/new", type: :view do
     assign(:course, Course.new(
       :code => "MyString",
       :description => "MyString",
+      :section => "MyString",
       :user_id => 1
     ))
   end
@@ -15,6 +16,8 @@ RSpec.describe "courses/new", type: :view do
     assert_select "form[action=?][method=?]", courses_path, "post" do
 
       assert_select "input#course_code[name=?]", "course[code]"
+
+      assert_select "input#course_section[name=?]", "course[section]"
 
       assert_select "input#course_description[name=?]", "course[description]"
 
