98da3fba99f496177d5f3583bd247ea1a8b2efa3
Whatisinternet <joshteeter@gmail.com>
2015-04-02 13:36:35 -0400

    Sort for assignment questions

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,7 +1,7 @@
 module AssignmentsHelper
   def render_assignment_questions
     out_string = ""
-    @assignment.assignment_questions.each do |aq|
+    @assignment.assignment_questions.sort.each do |aq|
         out_string += (render :partial => 'assignments/questions', locals: {:assignment_question => aq}).to_s
     end
     out_string.html_safe
