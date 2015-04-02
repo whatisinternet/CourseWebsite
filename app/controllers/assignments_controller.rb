ab011da7e47163ac86c5818ebdf6df6a06bf1883
Whatisinternet <joshteeter@gmail.com>
2015-04-02 13:15:08 -0400

    Assignment index added

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,6 +1,10 @@
 class AssignmentsController < ApplicationController
   before_action :set_assignment, only: [:show]
 
+  def index
+    @assignments = Assignment.all
+  end
+
   def show
 
   end
---
+++ spec/views/assignments/index.erb_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "assignments/index", type: :view do
+  before(:each) do
+    @assignment = FactoryGirl.create(:assignment)
+    assign(:assignments, [@assignment, @assignment])
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(@assignment.name)
+    expect(rendered).to match(@assignment.base_info.html_safe)
+    expect(rendered).to match(@assignment.due.to_s)
+  end
+
+end
