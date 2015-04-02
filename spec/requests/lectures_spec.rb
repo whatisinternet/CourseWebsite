b859229ea40dbe69f9809326bcb0c9616304a019
Whatisinternet <joshteeter@gmail.com>
2015-04-02 19:35:08 -0400

    lecture page is good to go. Set details, and formatted nicely

--- spec/requests/lectures_spec.rb
+++ spec/requests/lectures_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Lectures", type: :request do
   describe "GET /lectures" do
-    it "works! (now write some real specs)" do
+    it "returns success" do
       get lectures_path
       expect(response).to have_http_status(200)
     end
--- spec/views/lectures/show.html.erb_spec.rb
+++ spec/views/lectures/show.html.erb_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 RSpec.describe "lectures/show", type: :view do
   before(:each) do
     @lecture = FactoryGirl.create(:lecture)
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(renderer)
+    @content = markdown.render(@lecture.content)
   end
 
   it "renders attributes in <p>" do
     render
     expect(rendered).to match(@lecture.name)
-    expect(rendered).to match(@lecture.content)
+    expect(rendered).to match(@content)
     expect(rendered).to match(@lecture.slides)
   end
 end
