2a8113ddc5d620569d1658e358384962c4ac8fc1
Whatisinternet <joshteeter@gmail.com>
2015-04-02 23:43:39 -0400

    Assets

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ ruby '2.2.1'
 gem 'rails', '4.2.1'
 gem 'pg'
 gem "slim-rails"
-gem 'turbolinks'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
 //= require foundation
-//= require foundation/foundation
-$(document).foundation();
+//= require_tree .
 $(function() {
   $(document).foundation();
 });
