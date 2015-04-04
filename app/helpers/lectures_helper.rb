7d93d623ed5cf47ad332c49e8dbbad6b8b81de49
Whatisinternet <joshteeter@gmail.com>
2015-04-04 16:31:48 -0400

    moved logic out of the view

--- app/helpers/lectures_helper.rb
+++ app/helpers/lectures_helper.rb
@@ -2,4 +2,15 @@ module LecturesHelper
   def get_course
     @course = Course.find_by_id(@lecture.course_id) if @lecture
   end
+
+  def display_style(lecture)
+    if lecture.lecture_date < Date.today
+      "&#x2713;"
+    elsif lecture.lecture_date == Date.today
+      "&#x21d2;"
+    else
+      "&#x2715;"
+    end
+  end
+
 end
