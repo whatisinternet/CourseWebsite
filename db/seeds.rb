711d36ffec0a9f0afa58df6454332311f2260391
Whatisinternet <joshteeter@gmail.com>
2015-04-04 20:08:47 -0400

    Attempting to get the deploy button to actually work. Removed db:create because it should never have been there.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'facotry_girl'
 require 'factory_girl_rails'
 require 'faker'
 puts "\n\n\n\n"
